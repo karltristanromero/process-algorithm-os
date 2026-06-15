195acde72577a57ffe22bff1bd48bbf8216a7249
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 17:24:24 +0800

    add MFU algorithm class with eviction rationale and tie-breaking logic

--- virtual_memory_simulation/algorithms/most_frequently_used.py
+++ virtual_memory_simulation/algorithms/most_frequently_used.py
@@ -3,3 +3,11 @@ Most Frequently Used (MFU) Page Replacement Algorithm.
 '''
 
 from base import PageReplacementAlgorithm, SimStep
+
+class MFU(PageReplacementAlgorithm):
+    '''
+    Evicts the page with the highest access frequency count.
+    Rationale: the most-used page has likely already served its purpose,
+    while a newly loaded page still needs time to be useful.
+    Ties are broken by LRU order (least recently used among tied pages).
+    '''
