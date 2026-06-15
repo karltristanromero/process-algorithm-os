4228b844011b955377dccaf00eed098d43fb5170
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 16:49:59 +0800

    add LFU class implementation with eviction logic description

--- virtual_memory_simulation/algorithms/least_frequently_used.py
+++ virtual_memory_simulation/algorithms/least_frequently_used.py
@@ -3,3 +3,9 @@ Least Frequently Used (LFU) Page Replacement Algorithm.
 '''
 
 from base import PageReplacementAlgorithm, SimStep
+
+class LFU(PageReplacementAlgorithm):
+    '''
+    Evicts the page with the lowest access frequency count.
+    Ties are broken by recency (least recently used among tied pages).
+    '''
