b50c6374fec40e84c178e6224b1bb70dba61584e
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 16:58:36 +0800

    implement simulation logic for LFU algorithm with hit and fault tracking

--- virtual_memory_simulation/algorithms/least_frequently_used.py
+++ virtual_memory_simulation/algorithms/least_frequently_used.py
@@ -21,3 +21,15 @@ class LFU(PageReplacementAlgorithm):
 
         frames = {}         # page -> frequency count
         lru_order = []      # recency tracker for tie-breaking (oldest at front)
+
+        for i, page in enumerate(reference_string, 1):
+            if page in frames:
+                # hit – increment frequency and update recency
+                frames[page] += 1
+                lru_order.remove(page)
+                lru_order.append(page)
+                self.hit_count += 1
+                self.steps.append(SimStep(i, page, self._snapshot(list(frames)), False))
+            else:
+                self.fault_count += 1
+                evicted = None
