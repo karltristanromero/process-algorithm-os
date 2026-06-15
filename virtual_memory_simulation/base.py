c32fbf6b98cc2e97dfe6dfce0050b8614e26995a
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 20:30:33 +0800

    fix: correct type hint assignment for steps in PageReplacementAlgorithm

--- virtual_memory_simulation/base.py
+++ virtual_memory_simulation/base.py
@@ -24,7 +24,7 @@ class PageReplacementAlgorithm(ABC):
 
     def __init__(self, num_frames: int = 4):
         self.num_frames = num_frames
-        self.steps = list[SimStep] = []
+        self.steps: list[SimStep] = []
         self.fault_count = 0
         self.hit_count = 0
 
