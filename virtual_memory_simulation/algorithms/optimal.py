5921ca276c6bc0f2da2250d6fc15c5c9c71aa87c
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 15:24:40 +0800

    add class definition for Optimal Page Replacement Algorithm with docstring

--- virtual_memory_simulation/algorithms/optimal.py
+++ virtual_memory_simulation/algorithms/optimal.py
@@ -3,3 +3,11 @@ Optimal (Belady's) Page Replacement Algorithm.
 '''
 
 from base import PageReplacementAlgorithm, SimStep
+
+class Optimal(PageReplacementAlgorithm):
+    '''
+    Evicts the page that will not be used for the longest time in the future.
+    Requires knowing the full reference list in advance (theoretical base case).
+    '''
+
+    
