8e3827238106e5b8a7661ca4524c748dd3e2e364
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 15:58:29 +0800

    refactor simulate method in LRU class to improve page hit handling

--- virtual_memory_simulation/algorithms/least_recent_used.py
+++ virtual_memory_simulation/algorithms/least_recent_used.py
@@ -22,3 +22,15 @@ class LRU(PageReplacementAlgorithm):
         self.hit_count = 0
 
         cache = OrderedDict()   # key = page, order = recency (end = most recent)
+
+        for i, page in enumerate(reference_string, 1):
+            if page in cache:
+                # hit – move to most-recently-used end
+                cache.move_to_end(page)
+                self.hit_count += 1
+                self.steps.append(SimStep(i, page, self.snapshot(list(cache)), False))
+            else:
+                self.fault_count += 1
+                evicted = None
+
+                
