0ffe696e42f3f3e4eaefc856dc181b22f698bd29
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 18:52:33 +0800

    add import statements for necessary algorithms and display in VMSimulator

--- simulator.py
+++ simulator.py
@@ -2,3 +2,7 @@
 VMSimulator — the main controller.
 Handles user input, selects algorithms, and coordinates display output.
 '''
+
+import random
+from display import Display
+from algorithms import FIFO, Optimal, LRU, LRUApproximation, LFU, MFU
