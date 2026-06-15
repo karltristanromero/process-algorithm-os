40c0d415cebe29a310274a47405136b53d6d67b3
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 15:53:55 +0800

    add detailed docstring for LRU class to clarify eviction logic

--- virtual_memory_simulation/algorithms/least_recent_used.py
+++ virtual_memory_simulation/algorithms/least_recent_used.py
@@ -4,3 +4,10 @@ Least Recently Used (LRU) Page Replacement Algorithm.
 
 from collections import OrderedDict
 from base import PageReplacementAlgorithm, SimStep
+
+class LRU(PageReplacementAlgorithm):
+    '''
+    Evicts the page that has not been used for the longest time.
+    Uses an OrderedDict to efficiently track recency - least recently used
+    page is always at the front, most recently used at the end.
+    '''
