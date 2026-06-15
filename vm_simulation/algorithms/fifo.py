f89cb40ae290ac0db7337d088034560ba98eb908
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 14:45:46 +0800

    add class definition and docstring for FIFO page replacement algorithm

--- vm_simulation/algorithms/fifo.py
+++ vm_simulation/algorithms/fifo.py
@@ -5,3 +5,8 @@ First In, First Out (FIFO) Page Replacement Algorithm.
 from collections import deque
 from base import PageReplacementAlgorithm, SimStep
 
+class FIFO(PageReplacementAlgorithm):
+    '''
+    Evicts the page that has been in memory the longest.
+    Uses a queue to tract insertion order.
+    '''
