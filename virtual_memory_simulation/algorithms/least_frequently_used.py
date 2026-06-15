584f261ae8cbf349bd913e38b8ce6a80935e3c06
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 16:52:15 +0800

    implement LFU class with simulation method and name property

--- virtual_memory_simulation/algorithms/least_frequently_used.py
+++ virtual_memory_simulation/algorithms/least_frequently_used.py
@@ -9,3 +9,15 @@ class LFU(PageReplacementAlgorithm):
     Evicts the page with the lowest access frequency count.
     Ties are broken by recency (least recently used among tied pages).
     '''
+
+    @property
+    def name(self):
+        return 'Least Frequently Used (LFU)'
+    
+    def simulate(self, reference_string: list[int]) -> list[SimStep]:
+        self.steps = []
+        self.fault_count = 0
+        self.hit_count = 0
+
+        frames = {} 
+        lru_queue = []
