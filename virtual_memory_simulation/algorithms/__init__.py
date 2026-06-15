6727467df681b5956950b9b14f5dac258e461aea
cnriveraa <rivera.cn18@gmail.com>
2026-06-15 17:50:12 +0800

    add algorithm imports to __init__.py for convenient access

--- virtual_memory_simulation/algorithms/__init__.py
+++ virtual_memory_simulation/algorithms/__init__.py
@@ -1,2 +1,9 @@
 # This file makes the algorithm folder a Python package.
 # Import all algorithm classes here for convenient access.
+
+from algorithms.first_in_first_out import FIFO
+from algorithms.optimal import Optimal
+from algorithms.least_recent_used import LRU
+from algorithms.least_recent_used_approximation import LRUApproximation
+from algorithms.least_frequently_used import LFU
+from algorithms.most_frequently_used import MFU
