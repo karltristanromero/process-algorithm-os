a0217fa40e3ff70940089998ebc1567744e6d58b
cnriveraa <rivera.cn18@gmail.com>
2026-06-25 01:02:29 +0800

    fix: correct image resizing filter from LANZCOS to LANCZOS in CompareAllScreen

--- virtual_memory_simulation/gui/simulation_screen.py
+++ virtual_memory_simulation/gui/simulation_screen.py
@@ -235,7 +235,7 @@ class CompareAllScreen(tk.Frame):
         bg_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "SIMULATION_BG.png")
         if os.path.exists(bg_path):
             img = Image.open(bg_path).resize(
-                (theme.WINDOW_WIDTH, theme.WINDOW_HEIGHT), Image.LANZCOS)
+                (theme.WINDOW_WIDTH, theme.WINDOW_HEIGHT), Image.LANCZOS)
             self._bg_image = ImageTk.PhotoImage(img)
             tk.Label(self, image=self._bg_image).place(x=0, y=0, relwidth=1, relheight=1)
 
