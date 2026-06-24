5444817e6fa3f538d06fef8b2ffa0295ac2cc67d
cnriveraa <rivera.cn18@gmail.com>
2026-06-24 22:51:19 +0800

    add simulate method to SimulationScreen

--- virtual_memory_simulation/gui/simulation_screen.py
+++ virtual_memory_simulation/gui/simulation_screen.py
@@ -198,3 +198,6 @@ class SimulationScreen(tk.Frame):
         pages = [random.randint(0, 9) for _ in range(15)]
         self._ref_entry.delete(0, tk.END)
         self._ref_entry.insert(0, " ".join(map(str, pages)))
+
+    def _simulate(self):
+        
