b04cfc056fbf1299ef0e2fa19d80c19f15452ed3
cnriveraa <rivera.cn18@gmail.com>
2026-06-24 22:54:17 +0800

    add reset functionality to SimulationScreen

--- virtual_memory_simulation/gui/simulation_screen.py
+++ virtual_memory_simulation/gui/simulation_screen.py
@@ -209,4 +209,6 @@ class SimulationScreen(tk.Frame):
         self._stats_bar.update_stats(algo.fault_count, algo.hit_count, algo.hit_rate)
 
     def reset(self):
-        
+        self._ref_entry.delete(0, tk.END)
+        self._trace_table.render([], NUM_FRAMES)
+        self._stats_bar.reset()
