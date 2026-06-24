5d46ae99b5e3831d2de1ad4417ac267115caf27c
cnriveraa <rivera.cn18@gmail.com>
2026-06-24 20:50:35 +0800

    add AlgorithmSelectScreen class with docstring for clarity

--- virtual_memory_simulation/gui/algorithm_select.py
+++ virtual_memory_simulation/gui/algorithm_select.py
@@ -8,3 +8,10 @@ from PIL import Image, ImageTk
 import os
 import gui.theme as theme
 from gui.widgets import RoundedButton
+
+
+class AlgorithmSelectScreen(tk.Frame):
+    '''
+    Screen where the user picks which page replacement algorithm to run.
+    Layout: 2-column grid of 6 algorithm buttons + centered Compare All button.
+    '''
