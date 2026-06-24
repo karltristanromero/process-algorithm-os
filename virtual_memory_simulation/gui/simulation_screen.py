1ff325a4353ff4581e2bca9ef58386fda489ebd3
cnriveraa <rivera.cn18@gmail.com>
2026-06-24 23:55:28 +0800

    add simulation method to CompareAllScreen for handling parsed input

--- virtual_memory_simulation/gui/simulation_screen.py
+++ virtual_memory_simulation/gui/simulation_screen.py
@@ -436,3 +436,8 @@ class CompareAllScreen(tk.Frame):
         pages = [random.randint(0, 9) for _ in range(15)]
         self._ref_entry.delete(0, tk.END)
         self._ref_entry.insert(0, " ".join(map(str, pages)))
+
+    def _simulate(self):
+        ref = self._parse_input()
+        if ref is None:
+            return
